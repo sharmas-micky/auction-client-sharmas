a1524e24c8b7aafb0cb1acd52d088b192c1c617e
sharmas-micky <82169761+sharmas-micky@users.noreply.github.com>
2022-10-07 22:55:04 +0530

    Add files via upload

---
+++ src/components/Home.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Home() {
+  const [userName, setUserName] = useState('');
+  const navigate = useNavigate();
+  let handleSubmit = (e)=>{
+    e.preventDefault()
+    localStorage.setItem('userName', userName);
+    navigate('/products')
+  }
+  return (
+    <div>
+      <form className="home__form" onSubmit={handleSubmit}>
+        <label htmlFor="username">Enter your username</label>
+        <input
+          type="text"
+          name="username"
+          className="home__input"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+          required
+          minLength={6}
+        />
+        <button className="home__cta">SIGN IN</button>
+      </form>
+    </div>
+  )
+}
+
+export default Home
